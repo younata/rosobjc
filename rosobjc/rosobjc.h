e1ace4b4b66143046b4f65a97786651e9211026c
Rachel Brindle <you@subluminal.net>
2013-06-17 22:15:04 -0700

    Initial Commit

---
+++ rosobjc/rosobjc.h
@@ -0,0 +1,13 @@
+//
+//  rosobjc.h
+//  rosobjc
+//
+//  Created by Rachel Brindle on 6/17/13.
+//  Copyright (c) 2013 Rachel Brindle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface rosobjc : NSObject
+
+@end
