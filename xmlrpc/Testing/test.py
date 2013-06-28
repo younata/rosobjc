02ab9011d9f7d635f4043f6a42b8daecf09c1a5a
Rachel Brindle <you@subluminal.net>
2013-06-27 23:50:21 -0700

    removed mulle xmlrpc...

--- xmlrpc/Testing/test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import xmlrpclib
-    
-server_url = 'http://localhost:2333/RPC2';
-server = xmlrpclib.Server(server_url);
-    
-result = server.sample.doStuff([ "<&uuml;>", "quote \" unquote \"", { "key" : "value" } ])
-print "Result:", result
